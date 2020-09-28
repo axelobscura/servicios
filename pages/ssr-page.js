148deff6ce3d0fbdc96260f4dc133dde7be07560
axelobscura <axosar@gmail.com>
2020-09-28 13:16:57 -0500

    Agreagndo navs

---
+++ pages/ssr-page.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Nav from '../components/Nav';
+
+const SSRPage = () => {
+    return(
+        <div>
+            <Nav />
+            <h1>hello</h1>
+        </div>
+    )
+};
+
+export default SSRPage;
